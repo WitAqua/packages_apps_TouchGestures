LMODroid/platform_packages_apps_TouchGestures

TouchGestures: Fix HW package imports
Signed-off-by: Mohammad Hasan Keramat J <ikeramat@protonmail.com>
Change-Id: I43490a9b44f088696bd38edf9c67cbcbbaba4628

src/com/android/touch/gestures/TouchscreenGestureSettings.java
@@ -34,8 +34,8 @@ import androidx.preference.Preference;
 import androidx.preference.PreferenceFragment;
 import androidx.preference.PreferenceManager;
 
-import com.android.internal.custom.hardware.LineageHardwareManager;
-import com.android.internal.custom.hardware.TouchscreenGesture;
+import com.android.internal.libremobileos.hardware.LineageHardwareManager;
+import com.android.internal.libremobileos.hardware.TouchscreenGesture;
 
 import com.android.touch.gestures.R;
 import com.android.touch.gestures.utils.ResourceUtils;

WitAqua/packages_apps_TouchGestures

src/com/android/touch/gestures/TouchscreenGestureSettings.java
@@ -34,8 +34,8 @@ import androidx.preference.Preference;
 import androidx.preference.PreferenceFragment;
 import androidx.preference.PreferenceManager;
 
-import com.android.internal.custom.hardware.LineageHardwareManager;
-import com.android.internal.custom.hardware.TouchscreenGesture;
+import com.android.internal.derp.hardware.LineageHardwareManager;
+import com.android.internal.derp.hardware.TouchscreenGesture;
 
 import com.android.touch.gestures.R;
 import com.android.touch.gestures.utils.ResourceUtils;